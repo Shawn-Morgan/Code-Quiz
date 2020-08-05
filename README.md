cc0dff5e0e4b6e720487ff9750e374ae5ef15ce9
shawnm7474 <shawnm74@gmail.com>
2020-08-04 23:06:06 -0500

    final cleanup for final submission

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # HW04-Web-APIs-Code-Quiz
-homework 4 code quiz
+## Shawn Morgan
+
+* ['Code-Quiz GitHub'](https://github.com/Shawn-Morgan/HW04-Web-APIs-Code-Quiz)
+* ['Code-Quiz GitHub IO'](https://github.com/Shawn-Morgan/HW04-Web-APIs-Code-Quiz/blob/master/index.html)
+
+### Summary
+Built Code Quiz prototype using html, javascript and css with bootstrap
+Created landing page in html which then links to and starts the quiz on another html page that references javascript with features like a countdown timer, score increment for correct answers, score decrement for incorrect answers and countdown timer penalties for wrong answers. Final score displayed.
+
+### Acceptance 
+
+* Create a landing page to start the quiz with a title and explanation of the quiz and penalties for not answering questions correctly
+* Add a 'Start Quiz' button
+* Create a timer attached to the 'Start Quiz' button with a starting value of 60 seconds
+* When button is clicked start timer with a reverse countdown from 60 seconds
+* Create a count down timer on the page to show time left
+* Create a list of code questions
+* If right answer is chosen, present a message of "that's correct!"
+* If wrong answer is chosen, present a message of "sorry, that's not correct!"
+* If the right answer is chosen, add 20 seconds to the timer
+* If the wrong answer is chosen, subtract 20 seconds from the timer
+* Once a question is answered, right or wrong, advance to the next question
+* Score will keep track of how many right answers and increment by 10 points starting at 75
+* Present Final Score
+
+### Project Features
+* Score that is hidden until last question
+* Timer that is displayed on each page underneath each question
+* Flex display for multiple devices accomodations
+* Array with questions
+
--- assets/questions.js
+++ assets/questions.js
@@ -4,13 +4,15 @@ const questionDictionary = [{question: "Commonly used data types DO NOT include:
                             {question: "Which one of these is a JavaScript package manager?", option1: "Node.js", option2: "npm", option3: "TypeScript", option4: "python", correct: 2},
                             {question: "Which tool can you use to ensure code quality?", option1: "Angular", option2: "jQuery", option3: "RequireJS", option4: "ESLint", correct: 4}];
 
-
-var localStorage = Window.localStorage
+//log high scores
+//var localStorage = Window.localStorage
 
 //holds question user is on
 var questionNum = 0;
 //holds the score
-var score = 0;
+var score = 75;
+//setting the inital high score target
+var highScore = 75;
 
 //variables associated with timer
 var currentTime = document.querySelector("#currentTime");
@@ -23,21 +25,19 @@ var secondsLeft = 60;
 // Holds interval time
 var holdInterval = 0;
 // Holds penalty time
-var penalty = 5;
+var penalty = 20;
 
 //when timer runs out - new Date represents the current date/time
 var expirationDate = new Date()
 expirationDate.setMinutes(expirationDate.getMinutes() + 1)
 var x = setInterval(function(){
-    document.getElementById("timer").innerHTML="time left " + Math.floor((expirationDate-new Date())/1000) + "s"
+    document.getElementById("timer").innerHTML="Time left " + Math.floor((expirationDate-new Date())/1000) + "s"
     if (new Date() > expirationDate) {
         document.getElementById("questionContainer").innerHTML="Times up"
         var timer = document.getElementById("timer")
         timer.style.display="none"
     }
 },10)
-
-
 
 function answerGiven(optionNumber) {
     
@@ -46,27 +46,29 @@ function answerGiven(optionNumber) {
     if (optionNumber === questionDictionary[questionNum].correct){
         //this is for handling correct responses
         //increase time or score
-        score++
+        score+=10
         expirationDate.setSeconds(expirationDate.getSeconds()+5)
         console.log("the score is: " + score);
         console.log("this is question number: " + questionNum);
         var createDiv = document.createElement("div");
         createDiv.setAttribute("id", "createDiv");
-        createDiv.textContent = "Nice job! That is correct! The answer is: " + questionDictionary[questionNum].correct;
+        createDiv.textContent = "Nice job! That is correct!"
         console.log(createDiv.textContent);
-        //alert(createDiv.textContent), alert("the score is: " + score), alert("this is question number: " + questionNum);  
+        alert(createDiv.textContent), alert("the score is: " + score);  
     }
-        else {//for handling incorrect responses}
+        else {
+        //this is for handling correct responses
         //decrease time or score 
-        score--
-        expirationDate.setSeconds(expirationDate.getSeconds()-5)
+        score-=10
+        expirationDate.setSeconds(expirationDate.getSeconds()-penalty)
         console.log("the score is: " + score);
         console.log("this is question number: " + questionNum);
         var createDiv = document.createElement("div");
         createDiv.setAttribute("id", "createDiv");
-        createDiv.textContent = "Oh sorry, that is not correct. The answer is: " + questionDictionary[questionNum].correct;
+        //createDiv.textContent = "Oh sorry, that is not correct. The answer is: " + questionDictionary[questionNum].correct;
+        createDiv.textContent = "Oh sorry, that is not correct."
         console.log(createDiv.textContent);
-        //alert(createDiv.textContent), alert("the score is: " + score), alert("this is question number: " + questionNum);  
+        alert(createDiv.textContent), alert("the score is: " + score);
     }
     questionNum++;
     //questionNum += 1;
@@ -83,7 +85,7 @@ function resetQuestion(){
         timer.style.display="none"
         var finalResults = document.getElementById("finalResults");
         finalResults.style.display="block"
-        finalResults.innerHTML="Score: " + score;
+        finalResults.innerHTML="Final score: " + score;
 
         //var highScores=localStorage.getItem("High Scores")
 
@@ -104,36 +106,28 @@ function resetQuestion(){
 }
 
 resetQuestion()
-//global variable for correct answers given
-//html element that reports correct or incorrect selection - start with display none
 
 
-//function showReults()
-
-//timer function
-//timer.addEventListener("click", function())
+//add ability to save initials for high score attempt
 /*
-   // set to zero to start
-    if (holdInterval === 0) {
-        holdInterval = setInterval(function () {
-            secondsLeft--;
-            currentTime.textContent = "Time: " + secondsLeft;
+document.getElementById("initial-clickbutton").addEventListener("click", saveScore)
 
-            if (secondsLeft <= 0) {
-                clearInterval(holdInterval);
-                Done();
-                currentTime.textContent = "Time's up!";
-            }
-        }, 1000);
+function saveScore() {
+    var userInitials = document.querySelector("#initials-input").value;
+    var finalResults = timer
+    var scoreObject = {initials: userInitials, score: finalResults}
+    var highScore = localStorage.setItem("highestscorelist", highestscorelist);
+    //var highestscorelist = highScore
+    
+    highScore.style.display="none"
+
+    if (highScore == null) {
+        localStorage.getItem("highestscorelist",JSON.stringify([scoreObject]));
+        console.log(highScore);
+    } else {
+        highscorelist=JSON.parse(highestscore);
+        highscorelist.push(scoreObject);
+        localStorage.getItem("highestscorelist",JSON.stringify(highscorelist))
     }
-    render(questionIndex);
-});
+}
 */
-
-
-
-
-
-
-
-
